18aa1308e42ffd91cbea46549fe68e1b33379fc5
Artem <Lipatiev.artemii@gmail.com>
2021-10-11 14:21:38 +0300

    Fix models

--- address_book/mainapp/models.py
+++ address_book/mainapp/models.py
@@ -1,10 +1,5 @@
-import re
 from django.db import models
-from django.core.exceptions import ValidationError
-'''
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
-'''
+from django.core.validators import RegexValidator
 
 '''
 Создаем модель для адресной книги: Users
@@ -16,12 +11,12 @@ URL (опциональное поле, с валидацией) +
 Изображение (опциональное поле, метод хранения произвольный) +
 '''
 
+
 # Функции:
 # AddUser
 # EditUser
 # DeleteUser
 # SearchUser
-
 
 class Users(models.Model):
     name = models.CharField(max_length=20, verbose_name='Имя пользователя')
@@ -30,24 +25,15 @@ class Users(models.Model):
     city = models.CharField(max_length=30, verbose_name='Город', blank=True)
     street = models.CharField(max_length=30, verbose_name='Улица', blank=True)
     url_users = models.URLField(max_length=200, verbose_name='URL пользователя', blank=True, unique=True)
-    phone = models.CharField(max_length=15, verbose_name='Номер телефона')
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{5,12}$',
+        message="Номер телефона необходимо вводить в формате «+999999999» Допускается от 5 до 12 цифр.")
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     image = models.ImageField(verbose_name='Изображение', blank=True)
     # pip3 install pillow - for ImageField
 
     def __str__(self):
         return self.surname
-
-    @staticmethod
-    def validate_phone_number(phone):
-        regex = r'^\+?1?\d{9,15}$'    # Пересмотреть регулярные выражения, фильтруют не так!
-        if not re.match(regex, phone):
-            raise ValidationError('Wrong phone number format.')
-
-    @staticmethod
-    def validate_username(url_users):
-        regex = r'^([a-zA-Z]{4}[\w]{1,16})$'    # Пересмотреть регулярные выражения, фильтруют не так!
-        if not re.match(regex, url_users):
-            raise ValidationError('Wrong URL format.')
 
     class Meta:
         unique_together = ('name', 'surname')
