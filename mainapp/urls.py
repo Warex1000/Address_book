d9c78d15187b25349c5dbd58d7aa01a8025f7912
Artem <Lipatiev.artemii@gmail.com>
2021-10-24 13:36:50 +0300

    Add edit pages

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = (
     path('', views.index, name="index"),
     path('profile/<str:pk>', views.contactProfile, name="profile"),
+    path('edit/<str:pk>', views.edit, name="edit"),
 )
--- mainapp/views.py
+++ mainapp/views.py
@@ -22,8 +22,14 @@ def contactProfile(request, pk):
     return render(request, 'mainapp/contact-profile.html', all_users_context)
 
 
+def edit(request, pk):
+    contact = Users.objects.get(id=pk)
+    all_users = Users.objects.all()
+    all_users_context = {'all_users': all_users, 'info': contact}
+    return render(request, 'mainapp/edit-user.html', all_users_context)
 
-# 39:30
+
+# 48:40
 
 # def delete(request, pk):
 #     user = Users.objects.get(id=pk)
@@ -40,35 +46,6 @@ def contactProfile(request, pk):
 #         all_users.append(users_info)
 #
 #     context = {'all_users': all_users}
-#     return render(request, 'mainapp/edit.html', context)
-
-
-# def edit(request, users_id):
-#     users_item = get_object_or_404(Users, pk=users_id)
-#     return render(request, 'mainapp/edit.html', {"users_item ": users_item })
-
-
-# def edit(request, user_id):
-#
-#     users = Users.objects.get(pk=user_id)
-#     all_users = []
-#
-#     for user in users:
-#         users_info = dict(name=user.name, surname=user.surname, image=user.image)
-#         all_users.append(users_info)
-#
-#     context = {'all_users': all_users, 'user': users}
-#     return render(request, 'mainapp/edit.html', context)
-
-
-# def edite(self, *, object_list=None, **kwargs):
-#     context = super().edite(**kwargs)
-#     context['users'] = Users.objects.)
-#     assert isinstance(context, Users.objects)
-#     return Users.objects.filter(category_id=self.kwargs['users'])
-
-
-
-
+#     return render(request, 'mainapp/edit-user.html', context)
 
 
